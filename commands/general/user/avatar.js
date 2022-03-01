64af887ef167b3b8902de29bdea2b8c8d5abc643
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-03-01 22:20:12 +0300

    Update avatar.js

--- commands/general/user/avatar.js
+++ commands/general/user/avatar.js
@@ -17,5 +17,5 @@ $title[✄ - - - - - - - - - - - - - - - -]
 $description[[─﹕๑ Avatar Link]($userAvatar[$mentioned[1;yes]])]
 $image[$replaceText[$userAvatar[$mentioned[1]];?size=4096?;?size=256;-1]]
 $footer[Asked by: $username#$discriminator[$authorID];$authorAvatar]
-$suppressErrors
+$suppressErrors`
 }
