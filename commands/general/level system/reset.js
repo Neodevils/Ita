845bad37931d85671c9ccca539049e43fc56e10a
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-03-01 22:14:41 +0300

    Create reset.js

---
+++ commands/general/level system/reset.js
@@ -0,0 +1,8 @@
+module.exports = {
+name: "lvl-reset",
+code: `
+Ouh, <@$mentioned[1;yes]>'s; lvl and xp got reset :/ me sad, sad. 
+$setUserVar[level;0;$mentioned[1;yes]]
+$setUserVar[req;250;$mentioned[1;yes]]
+$setUserVar[xp;0;$mentioned[1;yes]]`
+}
