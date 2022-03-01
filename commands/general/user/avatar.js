91ab0bf8e6e8ca816d5ea11cc5e3268234c3d134
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-03-01 22:19:54 +0300

    Create avatar.js

---
+++ commands/general/user/avatar.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: "avatar",
+	aliases: ['av'],
+	code: `
+$if[$message==;]
+$author[$username#$discriminator;$authorAvatar]
+$title[✄ - - - - - - - - - - - - - - - -]
+$color[$getServerVar[hex]]
+$description[[─﹕๑ Avatar Link]($authorAvatar)]
+$image[$replaceText[$authorAvatar;?size=4096?;?size=256;-1]]
+$footer[Asked by: $username#$discriminator[$authorID];$authorAvatar]
+$suppressErrors
+$else
+
+$author[$user[$mentioned[1];tag];$userAvatar[$mentioned[1]]]
+$title[✄ - - - - - - - - - - - - - - - -]
+$description[[─﹕๑ Avatar Link]($userAvatar[$mentioned[1;yes]])]
+$image[$replaceText[$userAvatar[$mentioned[1]];?size=4096?;?size=256;-1]]
+$footer[Asked by: $username#$discriminator[$authorID];$authorAvatar]
+$suppressErrors
+}
