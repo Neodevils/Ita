aa639515766f5cf26397dbaba769f83168a9c527
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-03-01 22:10:55 +0300

    Delete commands/general/general others directory

--- commands/general/general others/server-logo.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({
-name: "logo",
-aliases:['server-icon'],
-code: `$onlyIf[$message==;]
-$title[<a:0_flower1:817391078810320916> server icon<a:0_flower1:817391078810320916>;$serverIcon]
-$image[$serverIcon]
-$color[$getServerVar[hex]]` 
-})
