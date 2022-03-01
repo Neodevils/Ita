a527d535387a73e9f220cb271f03d0db4e719843
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-03-01 22:21:43 +0300

    Create eval.js

---
+++ commands/bot/eval.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name:"eval",
+  code:`$eval[$message]
+  $onlyForIDs[285118390031351809;{description:**⨯ Eval command only can be used __by Neo.__**}{color:$getServerVar[hex]}]`
+}
