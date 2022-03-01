4fdf0df1129801d98e0b731c91c756df278d8dc0
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-03-01 22:24:08 +0300

    Create strawberry_leaderboard.js

---
+++ commands/economy/about user/strawberry_leaderboard.js
@@ -0,0 +1,8 @@
+module.exports = {
+name: "lb-stawwy",
+code: `
+$title[**__<a:strawwy:861057831712980992> pocket leaderboard <a:strawwy:861057831712980992>__**]
+$description[$globalUserLeaderboard[pocket;asc;{top}⸝⸝ __{username}__ — <a:strawwy:861057831712980992> **{value}** stawwy]]
+$color[$getServerVar[hex]]
+$footer[୨୧ You have 🍓 $numberSeparator[$getGlobalUserVar[pocket;$authorID]] in your pocket!!;$authorAvatar]`
+}
