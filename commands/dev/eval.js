3039a861851ec589d8e30916a908335b656ca325
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-03-25 14:43:11 +0000

    Update and Eval changes.

--- commands/dev/eval.js
+++ commands/dev/eval.js
@@ -9,8 +9,6 @@ $onlyForIDs[285118390031351809;{"content":"<a:dp_star6:835142297125519420> ⨯ U
 
 $onlyBotPerms[embedlinks;{"content":"<a:dp_star6:835142297125519420> ⨯ Uhh... I need \`EMBED_LINKS\` permission.","options":{"interaction" : true}}]
 
-$onlyIf[$guildID!=;{"embeds" : "{newEmbed:{description: <a:dp_star6:835142297125519420> This application command doesn't work in dm <a:bun_question:817219210279256066>}{color:$getServerVar[hex]}}","options" : {"interaction" : true}}]
-
 $log[— $userTag, used Eval Slash Interaction :3
 ୨୧・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・୨୧]
 `
